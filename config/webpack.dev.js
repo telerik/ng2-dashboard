94c0cb30d147f7f0a27ab4bab6947542208e197b
Alex Gyoshev <alex@gyoshev.net>
2016-08-08 16:45:02 +0300

    chore: run server on all addresses

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -16,7 +16,7 @@ const DefinePlugin = require('webpack/lib/DefinePlugin');
 const ENV = process.env.ENV = process.env.NODE_ENV = 'development';
 const HMR = helpers.hasProcessFlag('hot');
 const METADATA = webpackMerge(commonConfig.metadata, {
-  host: 'localhost',
+  host: '0.0.0.0',
   port: 3000,
   ENV: ENV,
   HMR: HMR
