a99cda594c764871d04ab496db41da31e15a29df
joneff <joneff@gmail.com>
2016-09-16 18:02:15 +0300

    add toggle animation for navigation pane

--- web/app/app.component.ts
+++ web/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation, HostBinding } from '@angular/core';
+import { Component, ViewEncapsulation, HostBinding, HostListener, ViewChild, animate, state, style, transition, trigger } from '@angular/core';
 import { AppState } from './app.service';
 import { Observable } from 'rxjs/Rx';
 import { Router } from '@angular/router';
@@ -11,7 +11,13 @@ import { Router } from '@angular/router';
     ],
     template: `
         <div class="row">
-            <div id="nav" class="col-xs-3 bg-inverse p-y-3 k-vbox" *ngIf="showNav()">
+            <div id="top-bar" class="top-bar col-xs-12 bg-inverse hidden-md-up"></div>
+            <div id="nav" class="col-xs-3 bg-inverse p-y-3 k-vbox"
+                [@toggleNav]="navState" *ngIf="showNav()">
+                <span id="nav-toggle" class="nav-toggle hidden-md-up"
+                    (click)="toggleNav()">
+                    <span class="k-icon k-i-hamburger"></span>
+                </span>
                 <h1 id="app-title">Issues</h1>
                 <p id="app-subtitle">Sample Dashboard</p>
                 <hr />
@@ -43,21 +49,61 @@ import { Router } from '@angular/router';
                 <router-outlet></router-outlet>
             </main>
         </div>
-    `
+    `,
+    animations: [trigger(
+        'toggleNav',
+        [
+            state( 'collapsed, void', style({transform: 'translateX(-100%)'}) ),
+            state( 'expanded', style({transform: 'translateX(0)'}) ),
+            transition( 'collapsed <=> expanded',
+                [
+                    animate( 200 ),
+                    animate( 200 )
+                ]
+            )
+        ]
+    )]
 })
 export class App {
     private year = new Date().getFullYear();
-    constructor(private router: Router) {}
+    private navState: string;
+    constructor(private router: Router) {
+        if ( window.innerWidth < 768 ) {
+            this.navState = 'collapsed';
+        }
+        else {
+            this.navState = 'expanded';
+        }
+    }
 
     @HostBinding('attr.id') protected get id(): string {
         return 'app';
     }
 
     @HostBinding('class') protected get appClass(): string {
-        return 'app container-fluid'
+        return 'app container-fluid';
+    }
+
+    @HostListener('window:resize', ['$event'])
+    onResize(event) {
+        if ( event.target.innerWidth < 768 ) {
+            this.navState = 'collapsed';
+        }
+        else {
+            this.navState = 'expanded';
+        }
     }
 
     private showNav() {
         return this.router.url === '/signin' ? false : true;
+    }
+
+    private toggleNav() {
+        if ( this.navState === 'expanded' ) {
+            this.navState = 'collapsed'
+        }
+        else {
+            this.navState = 'expanded'
+        }
     }
 }
